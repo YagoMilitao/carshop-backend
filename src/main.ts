20c5dc577ef30b804ffd6c1671c243120d625e4f
YagoMilitao <yagolano@hotmail.com>
2026-01-04 22:26:03 -0300

    Configurar ValidationPipe global

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,19 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
+  /* Validação global:
+   whitelist remove campos extras, 
+   forbidNonWhitelisted rejeita request suspeita,
+   transform transforma tipos automaticamente (quando aplicável).
+  */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
