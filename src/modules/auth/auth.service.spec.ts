812518ea61105bdc1558cd02b61030db080b2f06
Yago Militão <yagolano@hotmail.com>
2026-03-30 20:54:25 -0300

    mock AuthService and JwtService in auth specs

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const jwtServiceMock = {
+    signAsync: jest.fn(),
+  };
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: jwtServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
